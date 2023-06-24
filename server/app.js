23880ec1f83df43fb1125d1668df575a08b24d89
hyeongyun <guddbs5545@naver.com>
2023-06-24 19:27:21 +0900

    백엔드 로그인 완성

--- server/app.js
+++ server/app.js
@@ -3,7 +3,8 @@ import cors from 'cors';
 import morgan from 'morgan';
 import helmet from 'helmet';
 import 'express-async-errors';
-import tweetRoute from './router/tweets.js';
+import tweetRouter from './router/tweets.js';
+import authRouter from './router/auth.js';
 
 const app = express();
 app.use(express.json());
@@ -11,7 +12,8 @@ app.use(helmet());
 app.use(cors());
 app.use(morgan('tiny'));
 
-app.use('/tweets', tweetRoute);
+app.use('/tweets', tweetRouter);
+app.use('/auth', authRouter);
 
 // Not Found 전달
 app.use((req, res, next) => {
@@ -22,4 +24,5 @@ app.use((error, req, res, next) => {
     console.log(error);
     res.sendStatus(500);
 })
+
 app.listen(8081);
--- server/data/tweet.js
+++ server/data/tweet.js
@@ -2,7 +2,7 @@ let tweets = [
     {
         id: '1',
         des: "Hello World",
-        createAt: Date.now().toString(),
+        createAt: new Date().toString(),
         name: 'Bob',
         username: 'bob',
         url: ""
@@ -10,7 +10,7 @@ let tweets = [
     {
         id: '2',
         des: "Hello my mate",
-        createAt: Date.now().toString(),
+        createAt: new Date().toString(),
         name: 'Yun',
         username: 'Yun',
         url: ""
@@ -18,27 +18,40 @@ let tweets = [
 ]
 
 export async function getAll() {
-    return tweets;
+    return Promise.all(
+      tweets.map(async (tweet) => {
+        const { username, name, url } = await userRepository.findById(
+          tweet.userId
+        );
+        return { ...tweet, username, name, url };
+      })
+    );
   }
   
   export async function getAllByUsername(username) {
-    return tweets.filter((tweet) => tweet.username === username);
+    return getAll().then((tweets) =>
+      tweets.filter((tweet) => tweet.username === username)
+    );
   }
   
   export async function getById(id) {
-    return tweets.find((tweet) => tweet.id === id);
+    const found = tweets.find((tweet) => tweet.id === id);
+    if (!found) {
+      return null;
+    }
+    const { username, name, url } = await userRepository.findById(found.userId);
+    return { ...found, username, name, url };
   }
   
-  export async function create(text, name, username) {
+  export async function create(text, userId) {
     const tweet = {
-      id: Date.now().toString(),
+      id: new Date().toString(),
       text,
       createdAt: new Date(),
-      name,
-      username,
+      userId,
     };
     tweets = [tweet, ...tweets];
-    return tweet;
+    return getById(tweet.id);
   }
   
   export async function update(id, text) {
@@ -46,7 +59,7 @@ export async function getAll() {
     if (tweet) {
       tweet.text = text;
     }
-    return tweet;
+    return getById(tweet.id);
   }
   
   export async function remove(id) {
